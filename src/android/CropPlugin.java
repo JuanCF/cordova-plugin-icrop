2923c087acdc5661f93659a794568b1d83d20c17
Juan Carlos Flores <jcflores_martineznajam@hotmail.com>
2018-10-16 12:03:17 -0600

    Added logs to icrop plugin

--- src/android/CropPlugin.java
+++ src/android/CropPlugin.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 
 import com.yalantis.ucrop.UCrop;
 
@@ -14,6 +15,8 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+private static final String TAG = "ICROPlugin";
 
 import java.io.File;
 
@@ -27,6 +30,7 @@ public class CropPlugin extends CordovaPlugin {
       if (action.equals("cropImage")) {
           String imagePath = args.getString(0);
           String sourceType = args.getString(1);
+		  Log.i(TAG,"sourcetype"+sourceType);
           JSONObject jsonObject = new JSONObject(sourceType);
           String targetSourceType  = jsonObject.getString("sourceType");
 
